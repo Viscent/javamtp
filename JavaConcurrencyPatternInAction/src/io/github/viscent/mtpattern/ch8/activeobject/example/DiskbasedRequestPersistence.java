d0212609ad74889dd66e8a0b6bf95145c6c7a1f8
viscent <viscent.huang@gmail.com>
2017-01-15 16:19:42 +0800

    Fix:修复apply4Filename方法中对sectionNames访问的线程安全问题。

--- JavaConcurrencyPatternInAction/src/io/github/viscent/mtpattern/ch8/activeobject/example/DiskbasedRequestPersistence.java
+++ JavaConcurrencyPatternInAction/src/io/github/viscent/mtpattern/ch8/activeobject/example/DiskbasedRequestPersistence.java
@@ -9,7 +9,7 @@
 本代码可以从下URL下载：
 https://github.com/Viscent/javamtp
 http://www.broadview.com.cn/27006
-*/
+ */
 
 package io.github.viscent.mtpattern.ch8.activeobject.example;
 
@@ -30,136 +30,135 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.log4j.Logger;
 
 public class DiskbasedRequestPersistence implements RequestPersistence {
-	// 负责缓存文件的存储管理
-	private final SectionBasedDiskStorage storage = new SectionBasedDiskStorage();
-	private final Logger logger = Logger
-	    .getLogger(DiskbasedRequestPersistence.class);
+  // 负责缓存文件的存储管理
+  private final SectionBasedDiskStorage storage = new SectionBasedDiskStorage();
+  private final Logger logger = Logger
+      .getLogger(DiskbasedRequestPersistence.class);
 
-	@Override
-	public void store(MMSDeliverRequest request) {
-		// 申请缓存文件的文件名
-		String[] fileNameParts = storage.apply4Filename(request);
-		File file = new File(fileNameParts[0]);
-		try {
-			ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(
-			    file));
-			try {
-				objOut.writeObject(request);
-			} finally {
-				objOut.close();
-			}
-		} catch (FileNotFoundException e) {
-			storage.decrementSectionFileCount(fileNameParts[1]);
-			logger.error("Failed to store request", e);
-		} catch (IOException e) {
-			storage.decrementSectionFileCount(fileNameParts[1]);
-			logger.error("Failed to store request", e);
-		}
+  @Override
+  public void store(MMSDeliverRequest request) {
+    // 申请缓存文件的文件名
+    String[] fileNameParts = storage.apply4Filename(request);
+    File file = new File(fileNameParts[0]);
+    try {
+      ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(
+          file));
+      try {
+        objOut.writeObject(request);
+      } finally {
+        objOut.close();
+      }
+    } catch (FileNotFoundException e) {
+      storage.decrementSectionFileCount(fileNameParts[1]);
+      logger.error("Failed to store request", e);
+    } catch (IOException e) {
+      storage.decrementSectionFileCount(fileNameParts[1]);
+      logger.error("Failed to store request", e);
+    }
 
-	}
+  }
 
-	class SectionBasedDiskStorage {
-		private Deque<String> sectionNames = new LinkedList<String>();
-		/*
-		 * Key->value: 存储子目录名->子目录下缓存文件计数器
-		 */
-		private Map<String, AtomicInteger> sectionFileCountMap = new HashMap<String, AtomicInteger>();
-		private int maxFilesPerSection = 2000;
-		private int maxSectionCount = 100;
+  class SectionBasedDiskStorage {
+    private Deque<String> sectionNames = new LinkedList<String>();
+    /*
+     * Key->value: 存储子目录名->子目录下缓存文件计数器
+     */
+    private Map<String, AtomicInteger> sectionFileCountMap = new HashMap<String, AtomicInteger>();
+    private int maxFilesPerSection = 2000;
+    private int maxSectionCount = 100;
 		private String storageBaseDir = System.getProperty("java.io.tmpdir") + "/vpn";
-		
-		public SectionBasedDiskStorage() {
-			File dir = new File(storageBaseDir);
-			if (!dir.exists()) {
-				dir.mkdirs();
-			}
-		}
 
-		private final Object sectionLock = new Object();
+    public SectionBasedDiskStorage() {
+      File dir = new File(storageBaseDir);
+      if (!dir.exists()) {
+        dir.mkdirs();
+      }
+    }
 
-		public String[] apply4Filename(MMSDeliverRequest request) {
-			String sectionName;
-			int iFileCount;
-			boolean need2RemoveSection = false;
-			String[] fileName = new String[2];
-			synchronized (sectionLock) {
-				// 获取当前的存储子目录名
-				sectionName = this.getSectionName();
-				AtomicInteger fileCount;
-				fileCount = sectionFileCountMap.get(sectionName);
-				iFileCount = fileCount.get();
-				// 当前存储子目录已满
-				if (iFileCount >= maxFilesPerSection) {
-					if (sectionNames.size() >= maxSectionCount) {
-						need2RemoveSection = true;
-					}
-					// 创建新的存储子目录
-					sectionName = this.makeNewSectionDir();
-					fileCount = sectionFileCountMap.get(sectionName);
+    private final Object sectionLock = new Object();
 
-				}
-				iFileCount = fileCount.addAndGet(1);
+    public String[] apply4Filename(MMSDeliverRequest request) {
+      String sectionName;
+      int iFileCount;
+      String oldestSectionName = null;
+      String[] fileName = new String[2];
+      synchronized (sectionLock) {
+        // 获取当前的存储子目录名
+        sectionName = getSectionName();
+        AtomicInteger fileCount;
+        fileCount = sectionFileCountMap.get(sectionName);
+        iFileCount = fileCount.get();
+        // 当前存储子目录已满
+        if (iFileCount >= maxFilesPerSection) {
+          if (sectionNames.size() >= maxSectionCount) {
+            oldestSectionName = sectionNames.removeFirst();
+          }
+          // 创建新的存储子目录
+					sectionName = makeNewSectionDir();
+          fileCount = sectionFileCountMap.get(sectionName);
 
-			}
+        }
+        iFileCount = fileCount.addAndGet(1);
 
-			fileName[0] = storageBaseDir + "/" + sectionName + "/"
-			    + new DecimalFormat("0000").format(iFileCount) + "-"
-			    + request.getTimeStamp().getTime() / 1000 + "-" + request.getExpiry()
-			    + ".rq";
-			fileName[1] = sectionName;
+      }
 
-			if (need2RemoveSection) {
-				// 删除最老的存储子目录
-				String oldestSectionName = sectionNames.removeFirst();
-				this.removeSection(oldestSectionName);
-			}
+      fileName[0] = storageBaseDir + "/" + sectionName + "/"
+          + new DecimalFormat("0000").format(iFileCount) + "-"
+          + request.getTimeStamp().getTime() / 1000 + "-" + request.getExpiry()
+          + ".rq";
+      fileName[1] = sectionName;
 
-			return fileName;
-		}
+      if (null != oldestSectionName) {
+        // 删除最老的存储子目录
+        this.removeSection(oldestSectionName);
+      }
 
-		public void decrementSectionFileCount(String sectionName) {
-			AtomicInteger fileCount = sectionFileCountMap.get(sectionName);
-			if (null != fileCount) {
-				fileCount.decrementAndGet();
-			}
-		}
+      return fileName;
+    }
 
-		private boolean removeSection(String sectionName) {
-			boolean result = true;
-			File dir = new File(storageBaseDir + "/" + sectionName);
-			for (File file : dir.listFiles()) {
-				result = result && file.delete();
-			}
-			result = result && dir.delete();
-			return result;
-		}
+    public void decrementSectionFileCount(String sectionName) {
+      AtomicInteger fileCount = sectionFileCountMap.get(sectionName);
+      if (null != fileCount) {
+        fileCount.decrementAndGet();
+      }
+    }
 
-		private String getSectionName() {
-			String sectionName;
+    private boolean removeSection(String sectionName) {
+      boolean result = true;
+      File dir = new File(storageBaseDir + "/" + sectionName);
+      for (File file : dir.listFiles()) {
+        result = result && file.delete();
+      }
+      result = result && dir.delete();
+      return result;
+    }
 
-			if (sectionNames.isEmpty()) {
+    private String getSectionName() {
+      String sectionName;
+
+      if (sectionNames.isEmpty()) {
 				sectionName = this.makeNewSectionDir();
 
-			} else {
-				sectionName = sectionNames.getLast();
-			}
+      } else {
+        sectionName = sectionNames.getLast();
+      }
 
-			return sectionName;
-		}
+      return sectionName;
+    }
 
-		private String makeNewSectionDir() {
-			String sectionName;
-			SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmmss");
-			sectionName = sdf.format(new Date());
-			File dir = new File(storageBaseDir + "/" + sectionName);
-			if (dir.mkdir()) {
-				sectionNames.addLast(sectionName);
-				sectionFileCountMap.put(sectionName, new AtomicInteger(0));
-			} else {
-				throw new RuntimeException("Cannot create section dir " + sectionName);
-			}
+    private String makeNewSectionDir() {
+      String sectionName;
+      SimpleDateFormat sdf = new SimpleDateFormat("MMddHHmmss");
+      sectionName = sdf.format(new Date());
+      File dir = new File(storageBaseDir + "/" + sectionName);
+      if (dir.mkdir()) {
+        sectionNames.addLast(sectionName);
+        sectionFileCountMap.put(sectionName, new AtomicInteger(0));
+      } else {
+        throw new RuntimeException("Cannot create section dir " + sectionName);
+      }
 
-			return sectionName;
-		}
-	}
+      return sectionName;
+    }
+  }
 }
