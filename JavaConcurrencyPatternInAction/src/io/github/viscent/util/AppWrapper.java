7b89079a3f4336d258a97fc574f17919f4da67b4
viscent <viscent.huang@gmail.com>
2018-08-22 21:35:24 +0800

    improve

--- JavaConcurrencyPatternInAction/src/io/github/viscent/util/AppWrapper.java
+++ JavaConcurrencyPatternInAction/src/io/github/viscent/util/AppWrapper.java
@@ -24,53 +24,51 @@ import java.util.Date;
 
 public class AppWrapper {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		String[] passArgs = new String[args.length - 2];
-		passArgs = Arrays.copyOfRange(args, 3, args.length);
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] passArgs = new String[args.length - 2];
+        passArgs = Arrays.copyOfRange(args, 3, args.length);
 
-		int preSleep = Integer.valueOf(args[0]);
-		int postSleep = Integer.valueOf(args[1]);
-		String destClass = args[2];
-		
-		String stdInFile=System.getProperty("std.in");
-		if(null!=stdInFile){
-			System.setIn(new FileInputStream(new File(stdInFile)));
-		}
+        int preSleep = Integer.valueOf(args[0]);
+        int postSleep = Integer.valueOf(args[1]);
+        String destClass = args[2];
 
-		try {
-			Thread.sleep(preSleep);
-		} catch (InterruptedException e) {
-		}
-		
-	
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-		try {
-			Method method = Class.forName(destClass).getMethod("main",
-			    String[].class );
+        String stdInFile = System.getProperty("std.in");
+        if (null != stdInFile) {
+            System.setIn(new FileInputStream(new File(stdInFile)));
+        }
 
-			System.out.println("Started at:" + sdf.format(new Date()));
-			method.invoke(null, new Object[]{passArgs});
-		} catch (SecurityException e1) {
-			e1.printStackTrace();
-		} catch (NoSuchMethodException e1) {
-			e1.printStackTrace();
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Finished at:" + sdf.format(new Date()));
+        try {
+            Thread.sleep(preSleep);
+        } catch (InterruptedException e) {
+        }
 
-		try {
-			Thread.sleep(postSleep);
-		} catch (InterruptedException e) {
-		}
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        try {
+            Method method = Class.forName(destClass).getMethod("main",
+                    String[].class);
 
-	}
+            System.out.println("Started at:" + sdf.format(new Date()));
+            method.invoke(null, new Object[] { passArgs });
+        } catch (SecurityException e1) {
+            e1.printStackTrace();
+        } catch (NoSuchMethodException e1) {
+            e1.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished at:" + sdf.format(new Date()));
+
+        try {
+            Thread.sleep(postSleep);
+        } catch (InterruptedException e) {
+        }
+
+    }
 
 }
--- JavaConcurrencyPatternInAction/src/io/github/viscent/util/Debug.java
+++ JavaConcurrencyPatternInAction/src/io/github/viscent/util/Debug.java
@@ -10,22 +10,61 @@
 https://github.com/Viscent/javamtp
 http://www.broadview.com.cn/27006
 */
-
 package io.github.viscent.util;
 
+import java.io.PrintStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Debug {
-		private static ThreadLocal<SimpleDateFormat> sdfWrapper=new ThreadLocal<SimpleDateFormat>(){
-			@Override
-      protected SimpleDateFormat initialValue() {
-	      return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-      }
-			
-		};
-		public static void info(String message){
-			SimpleDateFormat sdf=sdfWrapper.get();
-			System.out.println('['+sdf.format(new Date())+"][INFO]["+Thread.currentThread().getName()+"]:"+message);
-		}
+  private static ThreadLocal<SimpleDateFormat> sdfWrapper = new ThreadLocal<SimpleDateFormat>() {
+    @Override
+    protected SimpleDateFormat initialValue() {
+      return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    }
+
+  };
+
+  enum Label {
+    INFO("INFO"),
+    ERR("ERROR");
+    String name;
+
+    Label(String name) {
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+  }
+
+  // public static void info(String message) {
+  // printf(Label.INFO, "%s", message);
+  // }
+
+  public static void info(String format, Object... args) {
+    printf(Label.INFO, format, args);
+  }
+
+  public static void info(boolean message) {
+    info("%s", message);
+  }
+
+  public static void info(int message) {
+    info("%d", message);
+  }
+
+  public static void error(String message, Object... args) {
+    printf(Label.ERR, message, args);
+  }
+
+  public static void printf(Label label, String format, Object... args) {
+    SimpleDateFormat sdf = sdfWrapper.get();
+    @SuppressWarnings("resource")
+    final PrintStream ps = label == Label.INFO ? System.out : System.err;
+    ps.printf('[' + sdf.format(new Date()) + "][" + label.getName()
+        + "]["
+        + Thread.currentThread().getName() + "]:" + format + " %n", args);
+  }
 }
